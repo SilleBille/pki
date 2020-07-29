4d660245dc340a155093e76b6faadeb504b575c3
Stanislav Levin <slev@altlinux.org>
2020-07-29 17:39:48 +0300

    Fix instance nssdb directory ownership
    There was a typo in code which sets the ownership
    of NSSdb directory and its content. This results
    in the group with the same gid as pkiuser uid
    can control this directory.
    
    Fixes: https://pagure.io/dogtagpki/issue/3195
    Signed-off-by: Stanislav Levin <slev@altlinux.org>

---
+++ base/server/upgrade/10.9.0/05-FixInstanceNSSDbFolderOwnership.py
@@ -0,0 +1,30 @@
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; version 2 of the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+
+from __future__ import absolute_import
+import os
+import pki.server.upgrade
+
+
+class FixInstanceNSSDbFolderOwnership(
+        pki.server.upgrade.PKIServerUpgradeScriptlet):
+
+    def __init__(self):
+        super(FixInstanceNSSDbFolderOwnership, self).__init__()
+        self.message = 'Fix instance nssdb directory ownership'
+
+    def upgrade_instance(self, instance):
+        directory = instance.nssdb_dir
+        if os.path.exists(directory):
+            pki.util.chown(directory, instance.uid, instance.gid)
